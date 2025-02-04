bdf652fda508d602f6e34a94dc97063fb0258ba0
Nick Williams <nwilliams@xyme.ai>
2025-02-04 14:13:52 +0000

    added enviroment.yml

--- run_scripts/retrieve_jobs_pocketgen.py
+++ run_scripts/retrieve_jobs_pocketgen.py
@@ -2,7 +2,7 @@ import json
 
 from src.applications.pocketgen.run_pocketgen import retrieve_pocketgen_jobs
 
-json_file_path ='output_pipeline/pocketgen_20250202_173549/job_info.json'
+json_file_path ='output_pipeline/pocketgen_20250202_175350/job_info.json'
 
 with open(json_file_path, 'r') as f:
     job_info_list = json.load(f)
